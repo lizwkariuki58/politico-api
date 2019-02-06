60570bb8ba2970dc32bc4e474c7db4d5f82abefe
Liz <lizwkariuki58@gmail.com>
2019-02-06 21:52:09 +0300

    Ensure that server structure is working by creating basic Hello World app

---
+++ app/version1/models.py
@@ -0,0 +1,6 @@
+class User:
+    pass
+class PoliticalParty:
+    pass
+class GovernmentOffice:
+    pass
--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 import os
 from app import create_app
 
-config_name = os.getenv('APP_SETTINGS')
+config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == '__main__':
     app.run()
