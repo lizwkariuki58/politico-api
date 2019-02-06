eda4338e17ecf8aeb7467d525cf7b1feb99f2297
Liz <lizwkariuki58@gmail.com>
2019-02-06 23:11:30 +0300

    Implement User can get a specific party endpoint

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify,abort
 import os
 from app import create_app
 
@@ -20,6 +20,13 @@ parties =[
 @app.route('/app/version1/parties', methods = ['GET'])
 def get_parties():
     return jsonify({'parties': parties})
+
+@app.route('/app/version1/parties/<int:party_id>',methods=['GET'])
+def get_party(party_id):
+    party = [party for party in parties if party['id'] == party_id]
+    if len(party) == 0:
+        abort(404)
+    return jsonify({'party': party[0]})
 
 if __name__ == '__main__':
     app.run()
