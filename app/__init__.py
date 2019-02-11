a7e075b39dba2dac928a0ed1fd7c8f9aea7c0a98
Liz <lizwkariuki58@gmail.com>
2019-02-11 23:00:12 +0300

    Restructure the Project by separating concerns

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+
+from flask import Flask,jsonify
+
 from instance.config import app_config
 
 def create_app(config_name):
@@ -7,5 +9,4 @@ def create_app(config_name):
     app.config.from_object(app_config[config_name])
     
     app.config.from_pyfile('config.py')
-    app.secret_key = os.getenv('SECRET')
     return app
--- app/version1/models.py
+++ app/version1/models.py
@@ -1,6 +1,28 @@
 class User:
     pass
-class PoliticalParty:
-    pass
+class Party:
+    parties = []
+    id = 0
+    name = ''
+    hqAddress = ''
+    slogan= ''
+
+    def __init__(self):
+        self.parties =[
+            {
+                'id': 1,
+                'name': 'Gryffindor',
+                'hqAddress': '4 Privet Drive',
+                'slogan':'Where dwell the brave at heart!'
+            },
+            {
+                'id': 2,
+                'name': 'Slytherin',
+                'hqAddress': '13 Spinners End'
+                'slogan':'Without cunning, there can be no innovation'
+            },
+
+        ]
+
 class GovernmentOffice:
     pass
