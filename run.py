47013170a12c8a222309556a762db7f60eb1003e
Liz <lizwkariuki58@gmail.com>
2019-02-11 23:06:26 +0300

    Restructure Project

--- run.py
+++ run.py
@@ -1,25 +1,10 @@
-from flask import jsonify
 import os
+
+from flask import jsonify,abort, make_response
+
 from app import create_app
 
 config_name = os.getenv('FLASK_ENV')
 app = create_app(config_name)
-
-parties =[
-    {'id':1,
-    'name': 'Gryffindor',
-    'hqAddress': '4 Privet Drive',
-    'logo_url': 'gryffindor.com/logo'},
-
-    {'id':2,
-    'name': 'Slytherin',
-    'hqAddress': '13 Spinners End',
-    'logo_url': 'slytherin.com/logo'},
-
-]
-@app.route('/app/version1/parties', methods = ['GET'])
-def get_parties():
-    return jsonify({'parties': parties})
-
 if __name__ == '__main__':
-    app.run()
+    app.run()s
