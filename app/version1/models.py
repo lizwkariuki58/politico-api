593745e0e665ff1b715152a79971a84525bae2d4
Liz <lizwkariuki58@gmail.com>
2019-02-12 16:00:35 +0300

    Use Flask Blueprints

--- app/version1/models.py
+++ app/version1/models.py
@@ -1,23 +1,23 @@
-
 class PoliticalParty():
     """ This class creates the blueprint for creating political parties. """
-
     parties=[]
    
-    def __init__(self, id, name, hqAddress, slogan):
-        self.id= id
+    def __init__(self, name=None, hqAddress=None, slogan=None):
+        self.id=len(self.parties)+1
         self.name= name
         self.hqAddress = hqAddress
         self.slogan= slogan
         
     def create_party(self):
-        party = {
-                'id': len(self.parties)+1,
-                'name': self.name,
-                'hqAddress': self.hqAddress,
-                'slogan':self.slogan
-            }
-        self.parties.append(party)
+       party = {
+           'id': len(self.parties)+1,
+           'name': self.name,
+           'hqAddress': self.hqAddress,
+           'slogan':self.slogan
+       }
 
+       self.parties.append(party)
+       return self.parties
+       
     def get_parties(self):
         return self.parties
--- app/version1/views.py
+++ app/version1/views.py
@@ -3,7 +3,8 @@ from flask import Blueprint, jsonify
 from app.version1.models import PoliticalParty
 
 parties_bp= Blueprint('parties',__name__)
-@parties_bp.route('/parties', methods = 'GET')
+
+@parties_bp.route('/parties', methods = ['GET'])
 def get_all_parties():
     party = PoliticalParty()
-    return party
+    return jsonify(party.get_parties())
