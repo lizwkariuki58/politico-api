f369bfab99344751c088e1f32236cdb014d8e0dc
Liz <lizwkariuki58@gmail.com>
2019-02-13 08:49:26 +0300

    Add functionality to create a Political Party

--- app/version1/models.py
+++ app/version1/models.py
@@ -1,33 +1,24 @@
-from flask import jsonify,request,make_response
-
 class PoliticalParty():
     """ This class creates the blueprint for creating political parties. """
 
     parties=[]
    
-    def __init__(self, id=0, name=None, hqAddress=None, slogan=None):
-        self.id= 0
+    def __init__(self,name=None, hqAddress=None, slogan=None):
+        self.id= len(self.parties)+1
         self.name= name
-        self.hqAddress = name
-        self.slogan= name
+        self.hqAddress = hqAddress
+        self.slogan= slogan
         
     def create_party(self):
-        req = request.get_json()
-        new ={
-        'id': len(self.parties)+1,
-        'name': req['name'],
-        'hqAddress':req['hqAddress'],
-        'slogan':req['slogan']
+        party= {
+            'id':len(self.parties)+1,
+            'name':self.name,
+            'hqAddress': self.hqAddress,
+            'slogan': self.slogan
         }
 
-        self.parties.append(new)
-        return make_response(jsonify({
-            'message':'Party Created',
-            'Party ID': new['id']
-        }),)
+        self.parties.append(party)
+        return self.parties
     
     def get_parties(self):
-        return make_response(jsonify({
-            'Message': 'OK',
-            'Parties': self.parties
-        }),200)
+        return self.parties
--- app/version1/views.py
+++ app/version1/views.py
@@ -1,34 +1,34 @@
-from flask import Blueprint, json,jsonify,request,make_response
-
+from flask import Blueprint,jsonify,request,make_response,abort
+import json
 from app.version1.models import PoliticalParty
 
 parties_bp= Blueprint('parties',__name__)
 
 @parties_bp.route('/parties', methods = ['POST'])
 def create_party():
-    party= PoliticalParty()
-    details = request.get_json()
-    name = details['name']
-    hqAddress = details['hqAddress']
-    slogan = details['slogan']
+    #Responses from the request.get_json request saved in a Python dictionary called data.
+    data = request.get_json()
 
-    new_party = {
-        'id' : len(party.parties)+1,
-        'name': name,
-        'hqAddress': hqAddress,
-        'slogan':slogan
-    }   
+    name = data['name']
+    hqAddress= data['hqAddress']
+    slogan= data['slogan']
 
-    party.parties.append(new_party) 
+     # Instance of PoliticalParty is created and the data above is unpacked as an argument
+    new=PoliticalParty(**data)
+
+    #The new party is added to the parties list. It is then made json serializable
+    new.parties.append(new)
+    new_dict = json.dumps(new.__dict__)
     return make_response(jsonify({
-        'Message': 'Party created successfully',
-        'Party Name': new_party['name']
+            'Party': new_dict,
+            'status':'OK'
+
     }))
 
 @parties_bp.route('/parties', methods = ['GET'])
 def get_all_parties():
-    party= PoliticalParty()
+    new= PoliticalParty()
     return make_response(jsonify({
-        'parties': party.parties,
+        'parties': new.parties,
         'status': 'OK'
     }),200)
