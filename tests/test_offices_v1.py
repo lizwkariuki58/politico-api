fa2aaa5a1e3fe6188f010a63ccc6d3746354d0cc
Liz <lizwkariuki58@gmail.com>
2019-02-18 19:28:56 +0300

    Test the functionality of /offices routes

--- tests/test_offices_v1.py
+++ tests/test_offices_v1.py
@@ -1,12 +1,13 @@
 from tests.base_test import BaseTest
-from tests.test_helpers import office1, office2, office3
+from tests.test_helpers import office1,office2,office3
 
 class TestOffices(BaseTest):
     def test_create_office(self):
-        result = self.client().post("api/v1/offices", data=office1)
+        result = self.client.post("api/v1/offices", data=office1)
         self.assertEqual(result.status_code,200)
 
     def test_get_all_offices(self):
-        self.client().post("/api/v1/offices", data=office1)
-        response = self.client().get("/api/v1/offices")
+        self.client.post("/api/v1/offices", data=office1)
+        self.client.post("/api/v1/offices", data=office2)
+        response = self.client.get("/api/v1/offices")
         self.assertEqual(response.status_code,200)
